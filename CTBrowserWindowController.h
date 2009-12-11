52457b7c5ad2156624add9b732346a7fcfe9b449
Danny Greg <danny@realmacsoftware.com>
2009-12-11 22:34:31 +0000

    Added in the browser window controller and xib templates.

---
+++ CTBrowserWindowController.h
@@ -0,0 +1,16 @@
+//
+//  CTBrowserWindowController.h
+//  Catalyst
+//
+//  Created by Danny Greg on 11/12/2009.
+//  Copyright 2009 Danny Greg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CTBrowserWindowController : NSWindowController {
+
+}
+
+@end
---
+++ CTBrowserWindowController.m
@@ -0,0 +1,14 @@
+//
+//  CTBrowserWindowController.m
+//  Catalyst
+//
+//  Created by Danny Greg on 11/12/2009.
+//  Copyright 2009 Danny Greg. All rights reserved.
+//
+
+#import "CTBrowserWindowController.h"
+
+
+@implementation CTBrowserWindowController
+
+@end
