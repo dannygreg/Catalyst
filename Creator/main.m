344844f44099c874ceb627a70f06d83269438c97
Danny Greg <danny@realmacsoftware.com>
2009-12-13 18:23:15 +0000

    Added the creator dir for the second target and a main.m

--- Creator/main.m
+++ Creator/main.m
@@ -24,8 +24,6 @@
 
 //*******************************************************************************
 
-#import <Cocoa/Cocoa.h>
-
 int main(int argc, char *argv[])
 {
     return NSApplicationMain(argc,  (const char **) argv);
